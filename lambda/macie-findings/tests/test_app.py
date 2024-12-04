c5e2ba5a08e164c652febf3d4ca8de5a9e28337c
Talha Zaman <chargeitothegame@Talhas-MBP.mynet>
2024-12-04 11:03:14 +0000

    Lambda Pipeline

--- lambda/macie-findings/tests/test_app.py
+++ lambda/macie-findings/tests/test_app.py
@@ -1,16 +1,16 @@
-from app import lambda_handler
-from moto import mock_aws
 import json
-import pytest
-import sys
 import os
+import sys
+import pytest
 import boto3
+from moto import mock_aws
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
 
-@mock_aws
+from app import lambda_handler
 
+@mock_aws
 def test_lambda_handler_success():
     # Set up mock AWS services
     sns = boto3.client('sns')
@@ -26,7 +26,10 @@ def test_lambda_handler_success():
         Bucket='test-bucket',
         CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'}
     )
-
+    
+    # Set up mock Security Hub
+    security_hub.enable_security_hub()
+    
     # Mock event data
     event = {
         'detail': {
@@ -50,7 +53,6 @@ def test_lambda_handler_success():
     assert json.loads(response['body']) == 'Successfully processed Macie finding'
 
 @mock_aws
-
 def test_lambda_handler_invalid_event():
     # Set up AWS region
     os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'

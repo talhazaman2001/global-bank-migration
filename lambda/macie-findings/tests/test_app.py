2eb602e51ff1b2d9f8caf08a7f5f1f12cceedff0
Talha Zaman <chargeitothegame@Talhas-MBP.mynet>
2024-12-04 11:05:01 +0000

    Lambda Pipeline

--- lambda/macie-findings/tests/test_app.py
+++ lambda/macie-findings/tests/test_app.py
@@ -1,14 +1,14 @@
+from moto import mock_aws
+from app import lambda_handler
 import json
 import os
 import sys
 import pytest
 import boto3
-from moto import mock_aws
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
 
-from app import lambda_handler
 
 @mock_aws
 def test_lambda_handler_success():
